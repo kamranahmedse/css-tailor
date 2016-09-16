0924cd21770ccf44b68f3367a9428fe0cf9e9e3f
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-16 16:48:54 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var _ = require('lodash');
+
+
+var defaultOptions = {},
+    lookupRegex = /(?:(\bclass\b)\s*=\s*("[^"]*"|'[^']*'|[^"'<>\s]+)\s*)+/;
+
+module.exports = {
+
+    tailorContent: function (htmlContent, options) {
+
+    },
+
+    tailorPath: function (path, options) {
+
+    }
+};
