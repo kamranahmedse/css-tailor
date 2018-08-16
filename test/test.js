9c6e1d5d408efbbbd982b5c8924b5dc32f7785f5
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-16 16:47:25 +0400

    Fix failing travis build

--- test/test.js
+++ test/test.js
@@ -1,19 +1,16 @@
-"use strict";
-
-var assert = require('assert');
-var tailor = require('../');
-var fs = require('fs');
-var path = require('path');
+const assert = require('assert');
+const tailor = require('../');
+const fs = require('fs');
+const path = require('path');
 
 describe('tailor-js', function () {
-
   it('can generate css from html string', function (done) {
-    var generatedCss = tailor.generateCss('<div class="container pt30"></div>');
-    var expectedCss = {
+    const generatedCss = tailor.generateCss('<div class="container pt30"></div>');
+    const expectedCss = {
       minified: '.pt30{padding-top:30px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".pt30": {
+        '.pt30': {
           properties: [{
             property: 'padding-top',
             value: '30px'
@@ -29,14 +26,14 @@ describe('tailor-js', function () {
   });
 
   it('can optionally set all styles to important', function (done) {
-    var generatedCss = tailor.generateCss('<div class="container pt30"></div>', {
+    const generatedCss = tailor.generateCss('<div class="container pt30"></div>', {
       setImportant: true
     });
-    var expectedCss = {
+    const expectedCss = {
       minified: '.pt30{padding-top:30px !important;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".pt30": {
+        '.pt30': {
           properties: [{
             property: 'padding-top',
             value: '30px !important'
@@ -52,8 +49,8 @@ describe('tailor-js', function () {
   });
 
   it('returns empty object when there are no required properties', function (done) {
-    var generatedCss = tailor.generateCss('<div class="container"></div>');
-    var expectedCss = {
+    const generatedCss = tailor.generateCss('<div class="container"></div>');
+    const expectedCss = {
       minified: '',
       formatted: '',
       object: {}
@@ -66,30 +63,30 @@ describe('tailor-js', function () {
   });
 
   it('can read and assign units from selector', function (done) {
-    var generatedCss = tailor.generateCss('<div class="container w30em fs40"><span class="head fw600n"></span><span class="w40p"></span></div>');
-    var expectedCss = {
+    const generatedCss = tailor.generateCss('<div class="container w30em fs40"><span class="head fw600n"></span><span class="w40p"></span></div>');
+    const expectedCss = {
       minified: '.w30em{width:30em;}.fs40{font-size:40px;}.fw600n{font-weight:600;}.w40p{width:40%;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".w30em": {
+        '.w30em': {
           properties: [{
             property: 'width',
             value: '30em'
           }]
         },
-        ".fs40": {
+        '.fs40': {
           properties: [{
             property: 'font-size',
             value: '40px'
           }]
         },
-        ".fw600n": {
+        '.fw600n': {
           properties: [{
             property: 'font-weight',
             value: '600'
           }]
         },
-        ".w40p": {
+        '.w40p': {
           properties: [{
             property: 'width',
             value: '40%'
@@ -104,12 +101,12 @@ describe('tailor-js', function () {
   });
 
   it('can take HTML from file and generate CSS', function (done) {
-    var generatedCss = tailor.generatePathCss(__dirname + '/fixtures/demo-1.html');
-    var expectedCss = {
+    const generatedCss = tailor.generatePathCss(__dirname + '/fixtures/demo-1.html');
+    const expectedCss = {
       minified: '.w1200{width:1200px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
@@ -123,24 +120,24 @@ describe('tailor-js', function () {
   });
 
   it('can read HTML files from any directory depth and generate CSS', function (done) {
-    var generatedCss = tailor.generatePathCss(__dirname + '/fixtures/sample-dir-2/');
-    var expectedCss = {
+    const generatedCss = tailor.generatePathCss(__dirname + '/fixtures/sample-dir-2/');
+    const expectedCss = {
       minified: '.p40{padding:40px;}.mb30{margin-bottom:30px;}.w1200{width:1200px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".p40": {
+        '.p40': {
           properties: [{
             property: 'padding',
             value: '40px'
           }]
         },
-        ".mb30": {
+        '.mb30': {
           properties: [{
             property: 'margin-bottom',
             value: '30px'
           }]
         },
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
@@ -154,27 +151,27 @@ describe('tailor-js', function () {
   });
 
   it('can read HTML and generate CSS from an array of paths', function (done) {
-    var generatedCss = tailor.generatePathCss([
+    const generatedCss = tailor.generatePathCss([
       __dirname + '/fixtures/demo-1.html',
       __dirname + '/fixtures/sample-dir'
     ]);
-    var expectedCss = {
+    const expectedCss = {
       minified: '.w1200{width:1200px;}.p40{padding:40px;}.mb30{margin-bottom:30px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
           }]
         },
-        ".p40": {
+        '.p40': {
           properties: [{
             property: 'padding',
             value: '40px'
           }]
         },
-        ".mb30": {
+        '.mb30': {
           properties: [{
             property: 'margin-bottom',
             value: '30px'
@@ -193,24 +190,24 @@ describe('tailor-js', function () {
     tailor.pushHtml('<div class="container mb40"></div>');
     tailor.pushPath(__dirname + '/fixtures/demo-1.html');
 
-    var generatedCss = tailor.generateLazy();
-    var expectedCss = {
+    const generatedCss = tailor.generateLazy();
+    const expectedCss = {
       minified: '.w1200{width:1200px;}.pt30{padding-top:30px;}.mb40{margin-bottom:40px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
           }]
         },
-        ".pt30": {
+        '.pt30': {
           properties: [{
             property: 'padding-top',
             value: '30px'
           }]
         },
-        ".mb40": {
+        '.mb40': {
           properties: [{
             property: 'margin-bottom',
             value: '40px'
@@ -227,29 +224,29 @@ describe('tailor-js', function () {
 
   it('can generate minified CSS file', function (done) {
 
-    var outputFilePath = __dirname + '/fixtures/sample-dir/assets/css/tailored.min.css',
+    const outputFilePath = __dirname + '/fixtures/sample-dir/assets/css/tailored.min.css',
       generatedCss = tailor.generatePathCss(__dirname + '/fixtures/', {
         outputPath: outputFilePath,
         minifyOutput: true
       });
 
-    var expectedCss = {
+    const expectedCss = {
       minified: '.w1200{width:1200px;}.p40{padding:40px;}.mb30{margin-bottom:30px;}',
       formatted: '[Not Adding - It would be messy to add here]',
       object: {
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
           }]
         },
-        ".p40": {
+        '.p40': {
           properties: [{
             property: 'padding',
             value: '40px'
           }]
         },
-        ".mb30": {
+        '.mb30': {
           properties: [{
             property: 'margin-bottom',
             value: '30px'
@@ -258,10 +255,10 @@ describe('tailor-js', function () {
       }
     };
 
-    var lstat = fs.lstatSync(outputFilePath);
+    const lstat = fs.lstatSync(outputFilePath);
     assert.equal(lstat.isFile(), true);
 
-    var generatedContent = fs.readFileSync(outputFilePath);
+    const generatedContent = fs.readFileSync(outputFilePath);
     assert.equal(generatedContent, expectedCss.minified);
 
     // Remove any existing generated file
@@ -274,29 +271,29 @@ describe('tailor-js', function () {
 
   it('can generate formatted CSS file', function (done) {
 
-    var outputFilePath = __dirname + '/fixtures/sample-dir/assets/css/tailored.css',
+    const outputFilePath = __dirname + '/fixtures/sample-dir/assets/css/tailored.css',
       generatedCss = tailor.generatePathCss(__dirname + '/fixtures/', {
         outputPath: outputFilePath,
         minifyOutput: false
       });
 
-    var expectedCss = {
+    const expectedCss = {
       minified: '.w1200{width:1200px;}.p40{padding:40px;}.mb30{margin-bottom:30px;}',
       formatted: '.w1200 {\n    width: 1200px;\n}\n\n.p40 {\n    padding: 40px;\n}\n\n.mb30 {\n    margin-bottom: 30px;\n}\n\n',
       object: {
-        ".w1200": {
+        '.w1200': {
           properties: [{
             property: 'width',
             value: '1200px'
           }]
         },
-        ".p40": {
+        '.p40': {
           properties: [{
             property: 'padding',
             value: '40px'
           }]
         },
-        ".mb30": {
+        '.mb30': {
           properties: [{
             property: 'margin-bottom',
             value: '30px'
@@ -305,10 +302,10 @@ describe('tailor-js', function () {
       }
     };
 
-    var lstat = fs.lstatSync(outputFilePath);
+    const lstat = fs.lstatSync(outputFilePath);
     assert.equal(lstat.isFile(), true);
 
-    var generatedContent = fs.readFileSync(outputFilePath);
+    const generatedContent = fs.readFileSync(outputFilePath);
     assert.equal(generatedContent, expectedCss.formatted);
 
     // Remove any existing generated file
